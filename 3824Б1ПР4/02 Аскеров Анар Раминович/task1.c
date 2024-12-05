8d0cc7d651948a0686662c875caa3769ffa6a499
Askerov177 <askerovanar32@gmail.com>
2024-12-05 10:18:03 +0300

    Update task1.c

--- 3824Б1ПР4/02 Аскеров Анар Раминович/task1.c
+++ 3824Б1ПР4/02 Аскеров Анар Раминович/task1.c
@@ -1,7 +1,14 @@
 // Задача 1. Найти сумму целых чисел a и b.
 // Если a + b >= INT_MAX, возвращать INT_MAX
 // Если a + b <= INT_MIN, возвращать INT_MIN
-int sum_of_two_numbers(int a, int b)
-{
-  return 0;
+#include <limits.h>
+int sumoftwo_numbers(int a, int b) {
+    // Проверяем возможное переполнение при сложении
+    if (b > 0 && a > INT_MAX - b) {
+        return INTMAX; // Если сумма превышает INTMAX, возвращаем INT_MAX
+    }
+    if (b < 0 && a < INT_MIN - b) {
+        return INTMIN; // Если сумма ниже INTMIN, возвращаем INT_MIN
+    }
+    return a + b; // Возвращаем обычную сумму
 }
