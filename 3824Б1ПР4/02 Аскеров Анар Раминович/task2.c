ac5a54e7c58421b97ba5355ec2d39dd8c3b03850
Askerov177 <askerovanar32@gmail.com>
2024-12-05 10:20:45 +0300

    Update task2.c

--- 3824Б1ПР4/02 Аскеров Анар Раминович/task2.c
+++ 3824Б1ПР4/02 Аскеров Анар Раминович/task2.c
@@ -1,6 +1,10 @@
 // Задача 2. Найти произведение натуральных чисел a и b
 // Если a * b >= UINT_MAX, возвращать UINT_MAX
-unsigned int product_of_two_numbers(unsigned int a, unsigned int b)
-{
-  return 0;
+#include <limits.h>
+unsigned int productoftwo_numbers(unsigned int a, unsigned int b) {
+    // Проверяем на переполнение
+    if (a > 0 && b > UINT_MAX / a) {
+        return UINTMAX; // Если произведение превышает UINTMAX, возвращаем UINT_MAX
+    }
+    return a * b; // В противном случае возвращаем произведение
 }
